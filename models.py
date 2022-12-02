b160bb5369e512ccc02c7e2cf15b6cef52ed3592
geymerson <geymerson.r@gmail.com>
2022-12-02 11:59:31 -0300

    hello world!

--- models.py
+++ models.py
@@ -6,12 +6,10 @@ class ModeloTarefa(db.Model):
     __tablename__ = "tarefas"
  
     id = db.Column(db.Integer, primary_key=True, unique = True)
-    # id_tarefa = db.Column(db.Integer(), unique = True)
     titulo = db.Column(db.String())
     status = db.Column(db.Integer())
  
     def __init__(self, titulo, status):
-        # self.id_tarefa = id_tarefa
         self.titulo = titulo
         self.status = status
  
